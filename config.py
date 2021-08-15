797252cde8e773a8201b6d3dac20232d9a469cb8
Ihsan <78595134+UserLazy@users.noreply.github.com>
2021-08-15 11:20:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,8 @@ que = {}
 SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME")
+QUE_IMG = getenv("QUE_IMG")
+AUD_IMG = getenv("AUD_IMG")
 
 admins = {}
 API_ID = int(getenv("API_ID"))
@@ -15,6 +17,6 @@ API_HASH = getenv("API_HASH")
 
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "10"))
 
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! \ .").split())
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
